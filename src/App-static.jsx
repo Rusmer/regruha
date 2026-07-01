ad5420a1c4a80a8774b778faf3587f117dacb114
Rusmers <120788756+Rusmer@users.noreply.github.com>
2026-07-01 14:17:10 +0300

    Create static version of App without Base44 authentication

---
+++ src/App-static.jsx
@@ -0,0 +1,25 @@
+import { Toaster } from "@/components/ui/toaster"
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClientInstance } from '@/lib/query-client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PageNotFound from './lib/PageNotFound';
+import ScrollToTop from './components/ScrollToTop';
+import Home from './pages/Home';
+
+// Static version of the app - no authentication needed
+function App() {
+  return (
+    <QueryClientProvider client={queryClientInstance}>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Toaster />
+      </Router>
+    </QueryClientProvider>
+  )
+}
+
+export default App
