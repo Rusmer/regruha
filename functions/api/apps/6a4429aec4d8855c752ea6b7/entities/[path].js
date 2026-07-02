ac25607024011abe76b3460caa58e70628734fbd
Rusmers <120788756+Rusmer@users.noreply.github.com>
2026-07-02 12:38:47 +0300

    Update [path].js

--- functions/api/apps/6a4429aec4d8855c752ea6b7/entities/[path].js
+++ functions/api/apps/6a4429aec4d8855c752ea6b7/entities/[path].js
@@ -1,7 +1,6 @@
 export async function onRequest(context) {
   const request = context.request;
   const url = new URL(request.url);
-
   url.hostname = "regruha-terminal-core.base44.app";
 
   return fetch(url.toString(), {
