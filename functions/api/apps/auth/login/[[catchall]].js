2c4a65848105d2fee2fee40fd5ab2d5074ab845a
Rusmers <120788756+Rusmer@users.noreply.github.com>
2026-07-03 16:50:12 +0300

    Delete functions/api/apps directory

--- functions/api/apps/auth/login/[[catchall]].js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.hostname = "regruha-terminal-core.base44.app";
-
-  return Response.redirect(url.toString(), 302);
-}
