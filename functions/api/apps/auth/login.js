b6045c517277a47338a137cd020506d594ff6777
Rusmers <120788756+Rusmer@users.noreply.github.com>
2026-09-15 19:02:59 +0300

    Update login.js

--- functions/api/apps/auth/login.js
+++ functions/api/apps/auth/login.js
@@ -1,18 +1,15 @@
 export async function onRequest(context) {
-  const requestUrl = new URL(context.request.url);
+  const url = new URL(context.request.url);
 
   const target = new URL(
     "https://app.base44.com/api/apps/auth/login"
   );
 
-  // Сохраняем app_id
-  const appId = requestUrl.searchParams.get("app_id");
+  target.searchParams.set(
+    "app_id",
+    "6a4429aec4d8855c752ea6b7"
+  );
 
-  if (appId) {
-    target.searchParams.set("app_id", appId);
-  }
-
-  // ВСЕГДА используем основной домен Base44
   target.searchParams.set(
     "from_url",
     "https://regruha-terminal-core.base44.app/"
